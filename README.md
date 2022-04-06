75f018de0f3ad03082a89a8cf1bc5f911f2ef92a
دانیال کفائی نژاد طهرانی <65863212+DaniyalKT@users.noreply.github.com>
2022-04-06 15:20:47 +0430

    firstCommit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Redis_Example
+در این پروژه مثالی ساده برای استفاده از Redis در پروژه Asp.net Core هستش 
---
+++ Redis-Example/Controllers/RedisController.cs
@@ -0,0 +1,38 @@
+﻿using EasyCaching.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Redis_Example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RedisController : ControllerBase
+    {
+        private readonly IEasyCachingProvider cachingProvider;
+        private readonly IEasyCachingProviderFactory _providerFactory;
+        public RedisController(IEasyCachingProviderFactory providerFactory)
+        {
+            _providerFactory = providerFactory;
+            this.cachingProvider = this._providerFactory.GetCachingProvider("redis1");
+        }
+
+        
+
+        [HttpGet("Set")]
+        public IActionResult SetItemInQuery()
+        {
+            this.cachingProvider.Set("KeyValue123", "here is my value", TimeSpan.FromDays(100));
+
+            return Ok();
+        }
+
+        [HttpGet("Get")]
+        public IActionResult GetItemInQuery()
+        {
+            var item = this.cachingProvider.Get<string>("KeyValue123");
+
+            return Ok(item);    
+        }
+    }
+}
